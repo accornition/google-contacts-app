cdffb13d778df4912c541276e0e8bc0a51090b2b
Vijay <accornition@gitlab.com>
2021-03-29 13:08:39 +0530

    Don't hardcode server url

--- server/frontend/js/index.js
+++ server/frontend/js/index.js
@@ -1,3 +1,7 @@
 window.onload = () => {
     localStorage.setItem("GOOGLE_CONTACTS_APP_SERVER_URL", window.location.origin);
 };
+
+function getFormAction(form) {
+    form.action = localStorage.getItem("GOOGLE_CONTACTS_APP_SERVER_URL") + "/auth/google";
+}
